2b07c3ba75475efcde67a0a326d56d57969dfcf5
Alailton-Jr <alailtonjunior@usp.br>
2024-07-03 11:21:40 -0300

    First

---
+++ src/protection/include/prot.hpp
@@ -0,0 +1,12 @@
+#ifndef PDISC_HPP
+#define PDISC_HPP
+
+
+
+void mult(int x, int y);
+
+
+
+
+
+#endif
---
+++ src/protection/src/pdis.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "prot.hpp"
+
+
+void mult(int x, int y){
+
+    std::cout << "The result of " << x << " * " << y << " is " << x * y << std::endl;
+
+}
